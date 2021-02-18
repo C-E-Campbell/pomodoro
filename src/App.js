f133521330777dc47844e0be061ceb95af27b434
charles <codelyfewithcharlie@gmail.com>
2021-02-18 08:01:32 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import moment from 'moment';
 
 function App() {
-  return <div></div>;
+  const [start25, setStart25] = useState(0);
+  const [end25, setEnd25] = useState(0);
+
+  const start25Timer = () => {
+    const newStartTime = moment().unix();
+    const endTime = moment().add(25, 'm').unix();
+    setStart25(newStartTime);
+    setEnd25(endTime);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          start25Timer();
+        }}
+      >
+        Start
+      </button>
+    </div>
+  );
 }
 
 export default App;
